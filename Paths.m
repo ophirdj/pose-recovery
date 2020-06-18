a3dc9a1696681e4c039c3ecb1ee4cb25d40c025c
Ophir de Jager <ophirdj@gmail.com>
2020-06-18 16:19:18 +0300

    constant bank paths

--- Paths.m
+++ Paths.m
@@ -2,42 +2,79 @@ addpath(genpath([pwd() '\..\MRepo\']));
 dir = 'C:\Users\Ophir\matlab_workspace\trajectories\';
 dtm_load();
 
+% Limit path generation
+sim_time_secs = 180;
+
+% Limit area of path generation
+margin = 3500;
+x_start = margin;
+x_end = size(DTM, 2) * cellsize - margin;
+y_start = margin;
+y_end = size(DTM, 1) * cellsize - margin;
+z_start = 1500;
+z_end = 2500;
+
+%Velocity variance
+V_horiz = 5;
+V_vert = 1;
+
 %% constant_velocity
+fail_cnt = 0;
 for n=1:10
     while 1
         name = sprintf('constant_velocity_%d', n);
-        sim_time_secs = 120;
-        ini_pos = [2000 + randi(5000); ...
-                   2000 + randi(5000); ...
-                   1500 + randi(1000)];
-        ini_vel = [5 * rand(); ...
-                   5 * rand(); ...
-                   1 * rand()];
-        if generate_line(ini_pos, ini_vel, sim_time_secs, name, dir, DTM, cellsize)
+        ini_pos = [x_start + randi(x_end-x_start); ...
+                   y_start + randi(y_end-y_start); ...
+                   z_start + randi(z_end-z_start)];
+        vel = [V_horiz * rand(); ...
+                   V_horiz * rand(); ...
+                   V_vert * rand()];
+        if constant_velocity(ini_pos, vel, sim_time_secs, name, dir, DTM, cellsize)
             fprintf('%s [Done]\n', name);
+            fail_cnt = 0;
             break;
+        else
+            fail_cnt = fail_cnt + 1;
+            fprintf('%s [FAILED] %d, retrying with different parameters...\n', name, fail_cnt);
         end
     end
 end
-% %% Circle_100_1
-% freq_Hz = 100;
-% ray_angles = [-pi/6:pi/180:pi/6 pi/6-pi/180:-pi/180:-pi/6+pi/180];
-% ini_pos = [4500 4500 1500]';
-% ini_vel = [10 0 0]';
-% mot_def = [6 pi/16 0 0 0 5000];
-% PATH = [dir 'Circle_100_1\'];
-% if ~PathGenAll(PATH, mot_def, ini_pos, ray_angles, freq_Hz, DTM, cellsize, ini_vel)
-%     fprintf('Error generating path\n');
-% else
-%     fprintf('Done %s\n', 'Line_100_1\');
-% end
 
+%% constant_bank
+for n=1:10
+    while 1
+        name = sprintf('constant_bank_%d', n);
+        ini_pos = [x_start + randi(x_end-x_start); ...
+                   y_start + randi(y_end-y_start); ...
+                   z_start + randi(z_end-z_start)];
+        ini_vel = [V_horiz * rand(); ...
+                   V_horiz * rand(); ...
+                   V_vert * rand()];
+        bank = (pi/32 + pi/64 * rand()) * power(-1, randi(2));
+        if constant_bank(ini_pos, ini_vel, bank, sim_time_secs, name, dir, DTM, cellsize)
+            fprintf('%s [Done]\n', name);
+            fail_cnt = 0;
+            break;
+        else
+            fail_cnt = fail_cnt + 1;
+            fprintf('%s [FAILED] %d, retrying with different parameters...\n', name, fail_cnt);
+        end
+    end
+end
 
 %% Supporting functions
-function [success] = generate_line(ini_pos, ini_vel, sim_time_secs, name, dir, DTM, cellsize)
+function [success] = constant_velocity(ini_pos, vel, sim_time_secs, name, dir, DTM, cellsize)
     freq_Hz = 100;
     ray_angles = [-pi/6:pi/180:pi/6 pi/6-pi/180:-pi/180:-pi/6+pi/180];
     mot_def = repmat([3 0 0 0 0 1], [sim_time_secs*freq_Hz 1]);
+    PATH = sprintf('%s%s\\', dir, name);
+    success = PathGenAll(PATH, mot_def, ini_pos, ray_angles, freq_Hz, DTM, cellsize, vel);
+end
+
+function [success] = constant_bank(ini_pos, ini_vel, bank_rads, sim_time_secs, name, dir, DTM, cellsize)
+    freq_Hz = 100;
+    ray_angles = [-pi/6:pi/180:pi/6 pi/6-pi/180:-pi/180:-pi/6+pi/180];
+    mot_def = [6 bank_rads 0 0 0 sim_time_secs];
     PATH = sprintf('%s%s\\', dir, name);
     success = PathGenAll(PATH, mot_def, ini_pos, ray_angles, freq_Hz, DTM, cellsize, ini_vel);
 end
